22d9e78189b0a944e52b861d9bf931796ffe5c2a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-06-22 18:50:58 -0500

    [feat] Card Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
-const Card = (props) => {
+const Card = ({ name, min, max, img, onClose }) => {
   // acá va tu código
-  return <div>Card Component</div>;
+  return (
+    <div>
+      <button onClick={onClose}>X</button>
+      <h4>{name}</h4>
+      <p>Min:</p>
+      <p>{min}</p>
+      <p>Max:</p>
+      <p>{max}</p>
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Temperatura de la ciudad" />
+    </div>
+  );
 };
 
 export default Card;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,5 @@
 const SearchBar = (props) => {
   // acá va tu código
-  // tip, puedes usar un map
   return <div>SearchBar Component</div>;
 };
 
