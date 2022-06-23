ed6d5570713694942bec127e3f3a1035cfceaaa0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-06-22 19:05:11 -0500

    [feat] Cards Component - renderizado condicional con return temprano

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,18 @@
+import Card from './Card';
+
 const Cards = (props) => {
   // acá va tu código
   // tip, puedes usar un map
-  return <div>Cards Component</div>;
+  if (!props.cities) {
+    return <h1>No hay ciudad</h1>;
+  }
+  return (
+    <div>
+      {props.cities.map((city) => (
+        <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
