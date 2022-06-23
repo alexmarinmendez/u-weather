86e3e14faa4f1d2093959bf45d25ef4e3d70b1a8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-06-22 19:10:14 -0500

    [feat] Cards Component - optional chaining

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,13 @@ import Card from './Card';
 const Cards = (props) => {
   // acá va tu código
   // tip, puedes usar un map
-  return <div>{props.cities ? props.cities.map((city) => <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />) : <h1>NO hay Ciudades</h1>}</div>;
+  return (
+    <div>
+      {props.cities?.map((city) => (
+        <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
