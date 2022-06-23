ecb5e829691d9b3918d1a9a6d088f6b757b1b22b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-06-22 19:06:34 -0500

    [feat] Cards Component - operador cortocircuito

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,16 +3,7 @@ import Card from './Card';
 const Cards = (props) => {
   // acá va tu código
   // tip, puedes usar un map
-  if (!props.cities) {
-    return <h1>No hay ciudad</h1>;
-  }
-  return (
-    <div>
-      {props.cities.map((city) => (
-        <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />
-      ))}
-    </div>
-  );
+  return <div>{props.cities && props.cities.map((city) => <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />)}</div>;
 };
 
 export default Cards;
