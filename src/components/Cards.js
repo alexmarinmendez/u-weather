2286e2778a688917b140cfa55074676d07228e10
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-06-22 19:08:08 -0500

    [feat] Cards Component - operador ternario

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,7 @@ import Card from './Card';
 const Cards = (props) => {
   // acá va tu código
   // tip, puedes usar un map
-  return <div>{props.cities && props.cities.map((city) => <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />)}</div>;
+  return <div>{props.cities ? props.cities.map((city) => <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />) : <h1>NO hay Ciudades</h1>}</div>;
 };
 
 export default Cards;
